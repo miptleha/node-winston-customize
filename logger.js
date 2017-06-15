22828d12d8c105dcad8e46b0012817613ed83565
Alexey Obukhov <miptleha@gmail.com>
2017-06-15 16:29:02 +0300

    Add files via upload

---
+++ logger.js
@@ -0,0 +1,32 @@
+var winston = require('winston');
+
+module.exports = function(callingModule) {
+
+    return new winston.Logger({
+        level: 'debug',
+        transports: [new winston.transports.Console({
+            label: getLabel(),
+            colorize: true,
+            timestamp: timestampFormat
+        }),
+        new winston.transports.File({
+            label: getLabel(),
+            filename: 'file.log',
+            json: false,
+            timestamp: timestampFormat
+        })]
+    });
+
+
+    // Output timestamp format
+    function timestampFormat() {
+        return new Date().toISOString().replace(/T/, ' ').replace(/Z/, '');
+    }
+
+    // Return the last folder name in the path and the calling module's filename.
+    function getLabel() {
+        var parts = callingModule.filename.split('\\');
+        var res = parts[parts.length - 2] + '/' + parts.pop();
+        return res;
+    }
+};
---
+++ test.js
@@ -0,0 +1,14 @@
+var logger = require('./logger')(module);
+
+logger.debug('debug info');
+
+logger.log('info', 'Hello distributed log files!');
+logger.info('Hello again distributed logs');
+
+logger.log('debug', 'Now my debug messages are written to console!');
+
+logger.profile('test');
+
+logger.info('CHILL WINSTON!', { seriously: true });
+
+logger.profile('test');
